1d6dd4f8b5194960e2dcde05bff3e529126c839a
jahfal21 <jahfalramadhan4@gmail.com>
2022-04-02 13:57:33 +0700

    first commit

--- modul6_kelompok_6/Program.cs
+++ modul6_kelompok_6/Program.cs
@@ -1,1 +1,13 @@
-﻿
+﻿using modul6_kelompok_6;
+
+class main
+{
+    public static void Main()
+    {
+        DataMahasiswa_1302204034 mhsA1 = new DataMahasiswa_1302204034();
+        mhsA1.ReadJSON();
+
+        //TeamMembers_1302204034 mhsA2 = new TeamMembers_1302204034();
+        // mhsA2.ReadJSON();
+    }
+}
