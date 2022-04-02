8e80dba54b3058c0028b2dc12b3db14ace8d31d8
luthfanrw17 <olioaglio55@gmail.com>
2022-04-02 15:25:04 +0700

    menambah class glossary item

--- modul6_kelompok_6/Program.cs
+++ modul6_kelompok_6/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using modul6_kelompok_6;
+
+class main
+{
+    public static void Main()
+    {
+        DataMahasiswa1302204038 mhsA1 = new DataMahasiswa1302204038();
+        mhsA1.ReadJSON();
+        Console.WriteLine();
+
+        TeamMembers_1302204038 mhsA2 = new TeamMembers_1302204038();
+        mhsA2.ReadJSON();
+        Console.WriteLine();
+
+        GlossaryItem_1302204038 mhsA3 = new GlossaryItem_1302204038();
+        mhsA3.ReadJSON();
+    }
+}
