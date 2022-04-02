07c3e08fda50e3873c2d0f0e1559130ff3a76e80
MUHAMMAD ABDURRAHMAN FIRDAUS <dawsss@365.telkomuniversity.ac.id>
2022-04-02 15:10:24 +0700

    Menmabahkan Class team member

--- modul6_kelompok_6/Program.cs
+++ modul6_kelompok_6/Program.cs
@@ -1,35 +1,27 @@
-﻿using Newtonsoft.Json;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
-
-namespace modul6_Kelompok_6
+﻿namespace modul6_Kelompok_6
 {
     internal class DataMahasiswa_1302204070
     {
-		public static void readJSON()
-		{
-			String jsonString = File.ReadAllText("C:\\Users\\ASUS\\source\\repos\\jurnal6_1_1302204070");
+        public static void readJSON()
+        {
+            String jsonString = File.ReadAllText("C:\\Users\\ASUS\\source\\repos\\jurnal6_1_1302204070");
 
-			dynamic data = JsonConvert.DeserializeObject(jsonString);
+            dynamic data = JsonConvert.DeserializeObject(jsonString);
 
 
 
-			Console.WriteLine("Nama : " + data.firstName + " " + data.lastName);
-			Console.WriteLine("Gender : " + data.gender);
-			Console.WriteLine("Umur : " + data.age);
-			Console.WriteLine("Alamat : " + data.address.streetAddress + ", " + data.address.city + ", " + data.address.state);
-			Console.WriteLine("Mata Kuliah : ");
-			foreach (var item in data.courses)
-			{
-				Console.WriteLine();
-				Console.WriteLine("Kode matkul : " + item.code);
-				Console.WriteLine("Nama matkul : " + item.name);
-			}
-			Console.WriteLine();
-		}
-	}
+            Console.WriteLine("Nama : " + data.firstName + " " + data.lastName);
+            Console.WriteLine("Gender : " + data.gender);
+            Console.WriteLine("Umur : " + data.age);
+            Console.WriteLine("Alamat : " + data.address.streetAddress + ", " + data.address.city + ", " + data.address.state);
+            Console.WriteLine("Mata Kuliah : ");
+            foreach (var item in data.courses)
+            {
+                Console.WriteLine();
+                Console.WriteLine("Kode matkul : " + item.code);
+                Console.WriteLine("Nama matkul : " + item.name);
+            }
+            Console.WriteLine();
+        }
+    }
 }
