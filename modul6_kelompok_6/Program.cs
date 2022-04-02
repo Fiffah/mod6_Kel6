36227cacd9a22bb81378c63dc24322bc8a86d776
ARZzxz <achmadrizky0211@gmail.com>
2022-04-02 14:12:11 +0700

    menambahkan class DataMahasiswa

--- modul6_kelompok_6/Program.cs
+++ modul6_kelompok_6/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace modul6_Kelompok_6
+{
+    internal class DataMahasiswa_1302200027
+    {
+
+    }
+}
