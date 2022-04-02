0f5da18eab7a79215dc2cc6ee9990c386e57e66e
SulthanFahrein <sulthanfahrein17@gmail.com>
2022-04-02 15:17:44 +0700

    Program

--- modul6_kelompok_6/Program.cs
+++ modul6_kelompok_6/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using modul6_kelompok_6;
+
+class main
+{
+    public static void Main()
+    {
+        DataMahasiswa_1302200121 mhsA1 = new DataMahasiswa_1302200121();
+        mhsA1.ReadJSON();
+        Console.WriteLine();
+
+        TeamMembers_1302200121 mhsA2 = new TeamMembers_1302200121();
+        mhsA2.ReadJSON();
+        Console.WriteLine();
+    }
+}
