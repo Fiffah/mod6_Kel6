9a3e48d77a39d90d97ffd4037f4f67d4b255e9a7
MUHAMMAD ABDURRAHMAN FIRDAUS <dawsss@365.telkomuniversity.ac.id>
2022-04-02 14:46:06 +0700

    Menambahkan class data mahasiswa

---
+++ DataMahasiswa_1302204070.cs
@@ -0,0 +1,25 @@
+﻿using System.Text.Json;
+
+namespace modul6_kelompok_6
+{
+	public class DataMahasiswa_1302204070
+	{
+		public class DataMahasiswa
+		{
+			public string firstName { get; set; }
+			public string lastName { get; set; }
+			public string gender { get; set; }
+			public string address { get; set; }
+			public string courses { get; set; }
+		}
+		public void ReadJSON()
+		{
+			string file = "jurnal6_1_1302204070.json";
+			string dataStr = File.ReadAllText(file);
+			DataMahasiswa dataMahasiswa = JsonSerializer.Deserialize<DataMahasiswa>(dataStr);
+
+
+			Console.WriteLine($"Nama depan {dataMahasiswa.firstName} nama belakang {dataMahasiswa.lastName} gender {dataMahasiswa.gender} beralamat di {dataMahasiswa.address} mengambil matkul {dataMahasiswa.courses}");
+		}
+	}
+}
